b87b349bbe194dd3873a87169d38ff6e3621b5c2
bbortolli <bbortolli2@hotmail.com>
2017-11-26 19:54:51 -0200

    Socket, Conexao, Montagem do pacote

--- daemon.py
+++ daemon.py
@@ -11,7 +11,31 @@ if len(sys.argv) < 2:
 else:
     PORT = int(sys.argv[1])
 
-def lerPacote(p):
+def execute(cmd): # Execucao dos comandos
+        if(cmd == 1):
+            return subprocess.check_output(['ps'])
+        elif(cmd == 2):
+            return subprocess.check_output(['df'])
+        elif(cmd == 3):
+            return subprocess.check_output(['finger'])
+        elif(cmd == 4):
+            return subprocess.check_output(['uptime'])
+        else:
+            return 'Erro'
+
+
+def conv(d): # Trabalha em cima de um binario, jogando para uma string
+            dados = (map(bin, bytearray(d)))
+
+            for x in range(len(dados)):
+                    dados[x] = dados[x] [2:].rjust(8, '0')
+    
+            dados = ''.join(dados)
+
+            return dados
+
+
+def lerPacote(p): #Fazer a leitura de cada parte do pacote
     Version =  p[0:4]
     IHL = p[4:8]
     TOS = p[8:16]
@@ -28,33 +52,44 @@ def lerPacote(p):
 
     return Protocol, TTL
 
-def montaPacote(cmd, t):
-
-    t = TCP_IP.split(".")
+def montaPacote(cmd, t): # Montar o pacote juntando cada parte
 
     Version =  '0010'
     IHL = '0101'
     TOS = '00000000'
-    Lenght = '0000000000000000'
     FragID = '0000000000000000'
     Flags = '111'
     FragOffset = '0000000000000'
-    TTL =
+    TTL = bin(int(t, 2)-1) [2:].rjust(8, '0')
     Protocol = '00000000'
     HeaderChecksum = '0000000000000000'
-    SourceAddr =
-    DestinationAddr =
-    Options =
 
-    return pacote
+    t = TCP_IP.split(".")
+    for i in range(len(t)):
+        t[i] = (bin(int(t[i]))) [2:].rjust(8, '0')
 
+    SourceAddr = ''.join(t)
+    DestinationAddr = ''.join(t)
 
-def execute(cmd):
-        if(cmd == 1):
-            return subprocess.check_output(['ps'])
-        elif(cmd == 2):
-            return subprocess.check_output(['df'])
-        elif(cmd == 3):
-            return subprocess.check_output(['finger'])
-        else:
-            return subprocess.check_output(['uptime'])
+    Dat = conv(execute(int(cmd, 2)))
+
+    tamPacote = (Version + IHL + TOS + Length + FragID + Flags + FragOffset + TTL + Protocol + HeaderChecksum \
+    + SourceAddr + DestinationAddr + Dat)
+
+    word32 = int(math.ceil(float(tamPacote) / 32.0))
+    Length = ''.join(bin(word32 * 32)) [2:].rjust(16, '0')
+
+    p = (Version + IHL + TOS + Length + FragID  + Flags + FragOffset + TTL + Protocol + HeaderChecksum \
+    + SourceAddr + DestinationAddr + Dat)
+
+    return p
+
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c.bind((IP, PORT))
+c.listen(300)
+
+while True:
+        connect, addr = c.accept()
+        print 'Addres = ', addr
+
+        thread.start_new_thread(Thread_funcao, (connect,))
