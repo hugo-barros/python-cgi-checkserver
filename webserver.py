893091d56677b37a8e681c185bee4abaa448e4ad
Hugo-Barros <hugomrbr@hotmail.com>
2017-11-28 11:27:56 -0200

    nova versao, ainda sem testes

--- webserver.py
+++ webserver.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 import cgi
 import cgitb
+import math
 
 IP = '127.0.0.1'
+buffer_size = 4000
+
+
+def send_and_wait_for_socket(port):
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((IP, port))
+		s.send(packet)
+
+		while True:
+			p = s.recv(buffer_size)
+
+			if not p:
+				break
+			else:
+				print lerPacote # todo : if checksum blablabla
+	finally:
+		s.close()
 
 def lerPacote(p): #Fazer a leitura de cada parte do pacote
     Version =  p[0:4]
@@ -23,30 +42,31 @@ def lerPacote(p): #Fazer a leitura de cada parte do pacote
 
 def montaPacote(cmd, Options='0'): # Montar o pacote juntando cada parte
 
-    Version =  '0010'
+    Version = '0010'
     IHL = '0101'
+    Length = '0000000000000000'
     TOS = '00000000'
     FragID = '0000000000000000'
     Flags = '000'
     FragOffset = '0000000000000'
     TTL = '00001111'
     Protocol = cmd
-    HeaderChecksum = funcao_crc16
+    HeaderChecksum = '0000000000000000' #funcao_crc16
 
-    # transformando um endereço de base 10 para base 2
+    # transformando um endereco de base 10 para base 2
     ip_bin = IP.split(".")
     for i in range(len(ip_bin)):
         ip_bin[i] = (bin(int(ip_bin[i]))) [2:].rjust(8, '0')
 
-    SourceAddr = ''.join(ip_bin) # endereço em binario
-    DestinationAddr = ''.join(ip_bin) # endereço em binario
-
+    SourceAddr = ''.join(ip_bin) # endereco em binario
+    DestinationAddr = ''.join(ip_bin) # endereco em binario
 
     tamPacote = len(Version + IHL + TOS + Length + FragID + Flags + FragOffset + TTL + Protocol + HeaderChecksum \
-    + SourceAddr + DestinationAddr + Dat)
+    + SourceAddr + DestinationAddr + Options)
 
     word32 = int(math.ceil(float(tamPacote) / 32.0))
     Length = ''.join(bin(word32 * 32)) [2:].rjust(16, '0')
+
 
     p = (Version + IHL + TOS + Length + FragID  + Flags + FragOffset + TTL + Protocol + HeaderChecksum \
     + SourceAddr + DestinationAddr + Options)
@@ -67,52 +87,93 @@ packet_list_maq1 = []
 packet_list_maq2 = []
 packet_list_maq3 = []
 
-if form["maq1_ps"] == 'ps':
-	packet_list_maq1.push(montaPacote('0001'))
+try:
+	if form["maq1_ps"].value == 'ps':
+		print ("</br> maq 1 ps entrou")
+		packet_list_maq1.append(montaPacote('0001'))
+except Exception:
+	pass
+
+try:
+	if form["maq1_df"].value == 'df':
+		print ("</br> maq 1 df entrou")
+		packet_list_maq1.append(montaPacote('0010'))
+except Exception:
+	pass
+
+try:
+	if form["maq1_finger"] == 'finger':
+		packet_list_maq1.push(montaPacote('0011'))
+except Exception:
+	pass
 
 
-if form["maq1_df"] == 'df':
-	packet_list_maq1.push(montaPacote('0010'))
+try:
+	if form["maq1_uptime"] == 'uptime':
+		packet_list_maq1.push(montaPacote('0100'))
+except Exception:
+	pass
+
+try:
+	if form["maq2_ps"] == 'ps':
+		packet_list_maq2.push(montaPacote('0001'))
+except Exception:
+	pass
+
+try:
+	if form["maq2_df"] == 'df':
+		packet_list_maq2.push(montaPacote('0010'))
+except Exception:
+	pass
+
+try:
+	if form["maq2_finger"] == 'finger':
+		packet_list_maq2.push(montaPacote('0011'))
+except Exception:
+	pass
+
+try:
+	if form["maq2_uptime"] == 'uptime':
+		packet_list_maq2.push(montaPacote('0100'))
+except Exception:
+	pass
+
+try:
+	if form["maq3_ps"] == 'ps':
+		packet_list_maq3.push(montaPacote('0001'))
+except Exception:
+	pass
+
+try: 
+	if form["maq3_df"] == 'df':
+		packet_list_maq3.push(montaPacote('0010'))
+except Exception:
+	pass
+
+try:
+	if form["maq3_finger"] == 'finger':
+		packet_list_maq3.push(montaPacote('0011'))
+except Exception:
+	pass
+
+try:
+	if form["maq3_uptime"] == 'uptime':
+		packet_list_maq3.push(montaPacote('0100'))
+except Exception:
+	pass
 
 
-if form["maq1_finger"] == 'finger':
-	packet_list_maq1.push(montaPacote('0011'))
-
-
-if form["maq1_uptime"] == 'uptime':
-	packet_list_maq1.push(montaPacote('0100'))
-
-
-if form["maq2_ps"] == 'ps':
-	packet_list_maq2.push(montaPacote('0001'))
-
-
-if form["maq2_df"] == 'df':
-	packet_list_maq2.push(montaPacote('0010'))
-
-
-if form["maq2_finger"] == 'finger':
-	packet_list_maq2.push(montaPacote('0011'))
-
-
-if form["maq2_uptime"] == 'uptime':
-	packet_list_maq2.push(montaPacote('0100'))
-
-
-if form["maq3_ps"] == 'ps':
-	packet_list_maq3.push(montaPacote('0001'))
-
-
-if form["maq3_df"] == 'df':
-	packet_list_maq3.push(montaPacote('0010'))
-
-
-if form["maq3_finger"] == 'finger':
-	packet_list_maq3.push(montaPacote('0011'))
-
-
-if form["maq3_uptime"] == 'uptime':
-	packet_list_maq3.push(montaPacote('0100'))
-
+print ("</br>packet_list_maq1: " + str(packet_list_maq1))
+print ("</br>packet_list_maq2: " + str(packet_list_maq2))
+print ("</br>packet_list_maq3: " + str(packet_list_maq3))
 
 # for que vai fazer as conexoes socket
+for packet in packet_list_maq1:
+	send_and_wait_for_socket(9001)
+
+for packet in packet_list_maq2:
+	send_and_wait_for_socket(9002)
+
+for packet in packet_list_maq3:
+	send_and_wait_for_socket(9003)
+
